05a2221f2767a99f0206690075693ee3c229e000
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-03 11:41:19 -0400

    effects player, add fx compiler

---
+++ Systems/Players/EffectsPlayer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CozmicVoid.Systems.Players
+{
+    public class EffectsPlayer : ModPlayer
+    {
+        public float screenFlash;
+        private float shakeDrama;
+
+        public void ShakeAtPosition(Vector2 position, float distance, float strength)
+        {
+            /*
+            LunarVeilClientConfig config = ModContent.GetInstance<LunarVeilClientConfig>();
+            if (!config.ShakeToggle)
+                return;*/
+            shakeDrama = strength * (1f - base.Player.Center.Distance(position) / distance) * 0.5f;
+        }
+
+        public override void ModifyScreenPosition()
+        {
+            if (shakeDrama > 0.5f)
+            {
+                shakeDrama *= 0.92f;
+                Vector2 shake = new Vector2(Main.rand.NextFloat(shakeDrama), Main.rand.NextFloat(shakeDrama));
+                Main.screenPosition += shake;
+            }
+        }
+    }
+}
