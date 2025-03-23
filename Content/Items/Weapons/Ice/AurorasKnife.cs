b3ee5be089ba5392e5d3908a8544078a96d43d93
T0rah <62261361+Torrahth@users.noreply.github.com>
2025-03-23 14:38:41 -0500

    snowflake ice knife

--- Content/Items/Weapons/Ice/AurorasKnife.cs
+++ Content/Items/Weapons/Ice/AurorasKnife.cs
@@ -1,72 +1,54 @@
-
-using Luminance.Assets;
 using Luminance.Core.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 using Terraria;
-using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
-using Terraria.Map;
 using Terraria.ModLoader;
 
-
-using Luminance.Common.DataStructures;
-using System.Collections.Generic;
-using static Terraria.GameContent.Animations.IL_Actions.Sprites;
-using NoxusBoss.Core.Graphics.Automators;
-using static System.Formats.Asn1.AsnWriter;
-
 namespace CozmicVoid.Content.Items.Weapons.Ice
-{ 
-	// This is a basic item template.
-	// Please see tModLoader's ExampleMod for every other example:
-	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
-	public class AurorasKnife : ModItem
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class AurorasKnife : ModItem
     {
 
 
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.CozmicVoid.hjson' file.
         public override void SetDefaults()
-		{
-			Item.damage = 10;
-			Item.DamageType = DamageClass.Melee;
-			Item.width = 40;
-			Item.height = 40;
-			Item.useTime = 20;
-			Item.useAnimation = 20;
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.knockBack = 6;
-			Item.value = Item.buyPrice(silver: 1);
-			Item.rare = ItemRarityID.Blue;
-			Item.UseSound = SoundID.Item1;
-			Item.autoReuse = true;
-			Item.shootSpeed = 12;
-			Item.shoot = ModContent.ProjectileType<AurorasKniferProj>();
-		}
-		public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.Register();
-		}
+        {
+            Item.damage = 10;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 6;
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shootSpeed = 12;
+            Item.shoot = ModContent.ProjectileType<AurorasKniferProj>();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
     }
-
-
-
-
-
-
-
-
-
 
 
     public class AurorasKniferProj : ModProjectile, IPixelatedPrimitiveRenderer
     {
-
+        public override string Texture => "CozmicVoid/Content/Items/Weapons/Ice/AurorasKnife";
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("Boralius");
@@ -76,15 +58,16 @@ namespace CozmicVoid.Content.Items.Weapons.Ice
         public override void SetDefaults()
         {
             Projectile.DamageType = DamageClass.Melee;
-            Projectile.aiStyle = -1;
+            Projectile.aiStyle = 1;
+            AIType = ProjectileID.Bullet;
             Projectile.width = 18;
-            Projectile.height = 46;
+            Projectile.height = 18;
             Projectile.penetrate += 1;
             Projectile.friendly = true;
         }
         public override void OnKill(int timeLeft)
         {
-
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<SnowflakeCore>(), Projectile.damage, Projectile.knockBack, Projectile.owner, 0);
 
         }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
@@ -125,30 +108,29 @@ namespace CozmicVoid.Content.Items.Weapons.Ice
         }
         public override void AI()
         {
-            Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f + 3.14f;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
         }
 
+        //  public override bool OnTileCollide(Vector2 oldVelocity)
+        //   {
+        //       var entitySource = Projectile.GetSource_FromThis();
+        //      for (int j = 0; j < 12; j++)
+        //       {
+        //          int a = Gore.NewGore(entitySource, new Vector2(Projectile.Center.X + Main.rand.Next(-10, 10), Projectile.Center.Y + Main.rand.Next(-10, 10)), Projectile.velocity, 911);
+        //         Main.gore[a].timeLeft = 20;
+        //          Main.gore[a].scale = Main.rand.NextFloat(.5f, 1f);
+        //      }
+        //      return base.OnTileCollide(oldVelocity);
+        //  }
 
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            var entitySource = Projectile.GetSource_FromThis();
-            for (int j = 0; j < 12; j++)
-            {
-                int a = Gore.NewGore(entitySource, new Vector2(Projectile.Center.X + Main.rand.Next(-10, 10), Projectile.Center.Y + Main.rand.Next(-10, 10)), Projectile.velocity, 911);
-                Main.gore[a].timeLeft = 20;
-                Main.gore[a].scale = Main.rand.NextFloat(.5f, 1f);
-            }
-            return base.OnTileCollide(oldVelocity);
-        }
 
-
-        public override void PostDraw(Color lightColor)
-        {
-            Color color = Color.White * 0.5f;
-            Texture2D texture = (Texture2D)Mod.Assets.Request<Texture2D>("Content/Items/Weapons/Ice/AurorasKniferProj");
-            Main.spriteBatch.Draw(texture, new Vector2(Projectile.position.X - Main.screenPosition.X + Projectile.width * 0.5f, Projectile.position.Y - Main.screenPosition.Y + Projectile.height - texture.Height * 0.5f + 2f),
-                new Rectangle(0, 0, texture.Width, texture.Height), color, Projectile.rotation, texture.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
-        }
+        //   public override void PostDraw(Color lightColor)
+        //   {
+        //      Color color = Color.White * 0.5f;
+        //      Texture2D texture = (Texture2D)Mod.Assets.Request<Texture2D>("Content/Items/Weapons/Ice/AurorasKnife");
+        //        Main.spriteBatch.Draw(texture, new Vector2(Projectile.position.X - Main.screenPosition.X + Projectile.width * 0.5f, Projectile.position.Y - Main.screenPosition.Y + Projectile.height - texture.Height * 0.5f + 2f),
+        //           new Rectangle(0, 0, texture.Width, texture.Height), color, Projectile.rotation, texture.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
+        //  }
 
         public int Time
         {
@@ -157,7 +139,132 @@ namespace CozmicVoid.Content.Items.Weapons.Ice
         }
 
 
-        public override string Texture => MiscTexturesRegistry.InvisiblePixelPath;
+        //public override string Texture => MiscTexturesRegistry.InvisiblePixelPath;
 
+    }
+    public class SnowflakeCore : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.aiStyle = -1;
+            Projectile.width = 26;
+            Projectile.height = 26;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 70;
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.scale = 0;
+        }
+        public override void AI()
+        {
+
+            if (Projectile.localAI[0] == 0)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, new Vector2(1, 0).RotatedBy(i * (Math.PI / 3)) / 10, ModContent.ProjectileType<SnowflakeSpike>(), Projectile.damage, Projectile.knockBack, Projectile.owner, Projectile.whoAmI, i);
+                }
+                Projectile.localAI[0] = 1;
+            }
+            if (Projectile.timeLeft <= 10)
+            {
+                Projectile.scale -= 0.1f;
+            }
+            else if (Projectile.scale < 1)
+            {
+                Projectile.scale += 0.1f;
+            }
+        }
+    }
+    public class SnowflakeSpike : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.timeLeft = 70;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.aiStyle = -1;
+            Projectile.hide = true;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.scale = 0;
+
+            var npc = Main.projectile[(int)Projectile.ai[0]];
+            Terraria.Audio.SoundEngine.PlaySound(SoundID.Grass, Projectile.Center);
+            Projectile.rotation = new Vector2(0, 35).RotatedBy(Projectile.ai[1] * (Math.PI / 3)).ToRotation();
+            Projectile.position = npc.Center;// + new Vector2(0, 35).RotatedBy(Projectile.rotation);
+            Projectile.position.X -= Projectile.width / 2;
+            Projectile.position.Y -= Projectile.height / 2;
+
+            Projectile.velocity = Vector2.Zero;
+        }
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            if (Projectile.ai[2] == 1f)
+            {
+                int npcIndex = (int)Projectile.ai[3];
+                if (npcIndex >= 0 && npcIndex < 200 && Main.npc[npcIndex].active)
+                {
+                    if (Main.npc[npcIndex].behindTiles)
+                    {
+                        behindNPCsAndTiles.Add(index);
+                    }
+                    else
+                    {
+                        behindNPCs.Add(index);
+                    }
+
+                    return;
+                }
+            }
+            behindProjectiles.Add(index);
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+            var offset = new Vector2(Projectile.width / 2f, Projectile.height / 2f);
+            var frame = texture.Frame(1, Main.projFrames[Projectile.type], 0, Projectile.frame);
+            Vector2 drawPos = (Projectile.position - Main.screenPosition) + offset - new Vector2(0, Projectile.width).RotatedBy(Projectile.rotation);
+            Color color = lightColor;
+            Main.EntitySpriteDraw(texture, drawPos, frame, color, Projectile.rotation, frame.Size() / 2, Projectile.scale, SpriteEffects.None, 0);
+            return false;
+        }
+
+        public override void AI()
+        {
+            if (Projectile.timeLeft >= 60)
+            {
+                var npc = Main.projectile[(int)Projectile.ai[0]];
+                Projectile.rotation = new Vector2(0, 45).RotatedBy(Projectile.ai[1] * (Math.PI / 3)).ToRotation();
+                Projectile.position = Vector2.Lerp(Projectile.Center, npc.Center + new Vector2(0, 45).RotatedBy(Projectile.rotation), 0.2f);
+                Projectile.position.X -= Projectile.width / 2;
+                Projectile.position.Y -= Projectile.height / 2;
+                Projectile.scale += 0.1f;
+            }
+            if (Projectile.timeLeft <= 20)
+            {
+
+                var npc = Main.projectile[(int)Projectile.ai[0]];
+                Projectile.position = Vector2.Lerp(Projectile.position, npc.Center, 0.05f);
+                Projectile.scale -= 0.05f;
+            }
+            if (Projectile.scale <= 0f && Projectile.timeLeft <= 10)
+            {
+                Projectile.Kill();
+            }
+
+        }
     }
 }
