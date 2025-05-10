a579a977eb1e60fd65824dfc06d3d9442c21bf0b
T0rah <62261361+Torrahth@users.noreply.github.com>
2025-05-10 18:40:00 -0500

    wa
    wa

---
+++ Content/Tiles/MoonTechLightT.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CozmicVoid.Content.Tiles
+{
+    public class MoonTechLightT : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            TileID.Sets.MultiTileSway[Type] = true;
+            TileID.Sets.IsAMechanism[Type] = true;
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTorch);
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.Origin = new Point16(1, 0);
+            TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, 2, 0);
+            TileObjectData.newTile.AnchorBottom = AnchorData.Empty;
+            TileObjectData.newTile.LavaDeath = true;
+            // Rather than many different items, the single item placing this tile places a random style.
+            TileObjectData.newTile.DrawYOffset = -2;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(235, 166, 135), Language.GetText("MapObject.Chandelier"));
+
+            // Since we are using RandomStyleRange without StyleMultiplier, we'll need to manually register the item drop for the tile styles other than style 0. Here we register the default drop for any style.
+            RegisterItemDrop(ModContent.ItemType<MoonTechLight>());
+
+            // Frozen style uses the temporary animation system to cycle between frames to give this style a flickering light effect when turning on.
+            //turningOnAnimationType = Animation.RegisterTemporaryAnimation(frameRate: 12, frames: [0, 2, 2, 3, 2, 2, 1, 3]);
+        }
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 1f;
+            g = 0.6f;
+            b = 0.8f;
+        }
+    } // add wire
+    public class MoonTechLight : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("A poisonous plant which weaves its way into entities");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100; // How many items are needed in order to research duplication of this item in Journey mode. See https://terraria.gamepedia.com/Journey_Mode/Research_list for a list of commonly used research amounts depending on item type.
+        }
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 10;
+            Item.useAnimation = 15;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.width = 20; // The item texture's width
+            Item.height = 20; // The item texture's height
+            Item.createTile = ModContent.TileType<MoonTechLightT>();
+            Item.maxStack = Item.CommonMaxStack; // The item's max stack value
+            Item.value = Item.buyPrice(copper: 40); // The value of the item in copper coins. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
+        }
+    }
+}
